87be9786ff6c2ef92c7725f67cf0d490fc042bd5
Taimoor23 <73740673+Taimoor23@users.noreply.github.com>
2022-09-17 18:01:35 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express=require('express')
+const { appendFileSync } = require('fs')
+const { receiveMessageOnPort } = require('worker_threads')
+const app=express()
+const server=require('http').Server(app)
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+
+const {v4:uuidv4}=require('uuid')
+app.get('/',(req,res)=>{
+    res.redirect(`/${uuidv4()}`)
+})
+app.get('/:room',(req,res)=>{
+    res.render('index',{roomid:req.params.room})
+})
+
+server.listen(3030)
